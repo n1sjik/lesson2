e58ee7b45e8f7789be2ac11c248a8cfbcc7bcf6a
n1sjik <kozlovna@reshenie-soft.ru>
2020-03-04 21:31:12 +0300

    new code

---
+++ src/lesson2/ArraysLesson.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class ArraysLesson {
+    public static void main(String[] args) {
+        // массивы ((((
+
+    }
+}
---
+++ src/lesson2/Loops.java
@@ -0,0 +1,67 @@
+package lesson2;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // цикл while
+        //while (условие если условие false то программа в цикл не заходит
+        // если true то выполняются все тело цикла до момента когда условие не становится false)
+        // {
+        // тело цикла
+        // }
+
+        /*Scanner in = new Scanner(System.in);
+                while (true) {
+                    System.out.println("введите целое число");
+                    int someNum = in.nextInt();
+                    if (someNum == 0) {
+                        System.out.println("выход из программы");
+                        break; // прерывание цикла (в таком виде только текущего цикла)
+                    }
+                    System.out.println(someNum * someNum);
+                }*/
+
+        /*Scanner in = new Scanner(System.in);
+        int someRandomNum = (int) (Math.random() * 6) + 1;
+        while (true) {
+            System.out.println("введите целое число от 1 до 6");
+            int someNum = in.nextInt();
+            if (someNum < someRandomNum) {
+                System.out.println("загаданное число больше");
+            }
+            else if (someNum > someRandomNum) {
+                System.out.println("загаданное число меньше");
+            }
+            else {
+                System.out.println("вы угадали");
+                break;
+            }
+            }*/
+
+        // цикл for
+        // for (счетчик (блок инициализации),(может быть несколько через ",");
+        // (условие),(может быть несколько через ",");
+        // обновление счетчиков (после выполнения тела),(может быть несколько через ","))
+        // {тело цикла}
+
+
+        // генерирует сама среда коммандой fori
+        // continue - переход на след итерацию
+
+
+        for (int i = 0; i < 600 ; i++) {
+            if (i % 1 == i) continue;
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+    }
+}
